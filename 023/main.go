26cd6140e48f4577678d6555348f3be863b27e9f
“wenming.zhang” <wenming.zhang@hp.com>
2021-02-27 11:35:06 +0800

    update 023

---
+++ 023/main.go
@@ -0,0 +1,21 @@
+package main
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+
+	n := len(lists)
+	for {
+		min := 0
+		for k, v := range lists {
+			if 
+
+		}
+
+	}
+    
+}
---
+++ 023/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type testCase struct {
+	s   int
+	ret []string
+}
+
+var testCases = []testCase{
+	//	{
+	//		s:   "()",
+	//		ret: true,
+	//	},
+	{
+		s:   3,
+		ret: []string{"((()))", "(()())", "(())()", "()(())", "()()()"},
+	},
+	{
+		s:   2,
+		ret: []string{"(())", "()()"},
+	},
+	//	{
+	//		s:   "(]",
+	//		ret: false,
+	//	},
+	//	{
+	//		s:   "(]",
+	//		ret: false,
+	//	},
+	//	{
+	//		s:   "([)]",
+	//		ret: false,
+	//	},
+	//	{
+	//		s:   "{[]}",
+	//		ret: true,
+	//	},
+}
+
+func TestIsValid(t *testing.T) {
+	for _, v := range testCases {
+		fmt.Println(v)
+		ret := GenerateParenthesisDP(v.s)
+		fmt.Println(ret)
+	}
+}
