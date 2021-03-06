aac4a0fd365c073bad66053680268cf150a6e54e
“wenming.zhang” <wenming.zhang@hp.com>
2021-03-06 21:39:04 +0800

    update

--- 023/main.go
+++ 023/main.go
@@ -1,21 +1,45 @@
 package main
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-func mergeKLists(lists []*ListNode) *ListNode {
 
+//Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func MergeKLists(lists []*ListNode) *ListNode {
+	first := true
 	n := len(lists)
-	for {
-		min := 0
-		for k, v := range lists {
-			if 
+	nil_count := 0
+	min_idx := 0
+	min_val := lists[0].Val
 
+	var head, tmp *ListNode
+	for {
+		for k, v := range lists {
+			if v == nil {
+				nil_count++
+				if nil_count == n {
+					break
+				}
+				continue
+			}
+			if v.Val <= min_val {
+				min_val = v.Val
+				min_idx = k
+			}
+		}
+		tmp = lists[min_idx]
+		if first {
+			head = tmp
+			first = false
+		}
+
+		if tmp != nil {
+			lists[min_idx] = tmp.Next
 		}
 
 	}
-    
+
+	return head
+
 }
--- 023/main_test.go
+++ 023/main_test.go
@@ -5,46 +5,54 @@ import (
 	"testing"
 )
 
-type testCase struct {
-	s   int
-	ret []string
-}
+func TestMergeKLists(t *testing.T) {
+	val1 := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	val2 := &ListNode{
+		Val:  4,
+		Next: nil,
+	}
+	val3 := &ListNode{
+		Val:  5,
+		Next: nil,
+	}
 
-var testCases = []testCase{
-	//	{
-	//		s:   "()",
-	//		ret: true,
-	//	},
-	{
-		s:   3,
-		ret: []string{"((()))", "(()())", "(())()", "()(())", "()()()"},
-	},
-	{
-		s:   2,
-		ret: []string{"(())", "()()"},
-	},
-	//	{
-	//		s:   "(]",
-	//		ret: false,
-	//	},
-	//	{
-	//		s:   "(]",
-	//		ret: false,
-	//	},
-	//	{
-	//		s:   "([)]",
-	//		ret: false,
-	//	},
-	//	{
-	//		s:   "{[]}",
-	//		ret: true,
-	//	},
-}
+	val11 := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	val12 := &ListNode{
+		Val:  3,
+		Next: nil,
+	}
+	val13 := &ListNode{
+		Val:  4,
+		Next: nil,
+	}
 
-func TestIsValid(t *testing.T) {
-	for _, v := range testCases {
-		fmt.Println(v)
-		ret := GenerateParenthesisDP(v.s)
-		fmt.Println(ret)
+	val21 := &ListNode{
+		Val:  2,
+		Next: nil,
+	}
+	val22 := &ListNode{
+		Val:  6,
+		Next: nil,
+	}
+
+	val1.Next = val2
+	val2.Next = val3
+
+	val11.Next = val12
+	val12.Next = val13
+
+	val21.Next = val22
+
+	lists := []*ListNode{val1, val11, val21}
+	fmt.Println("#################")
+	ret := MergeKLists(lists)
+	for ; ret != nil; ret = ret.Next {
+		fmt.Println(ret.Val)
 	}
 }
