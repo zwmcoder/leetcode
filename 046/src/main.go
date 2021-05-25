c2b435bd69f0cf4f681d96112c7f6bce24b89c6d
zwmcoder <zhangwm1987@163.com>
2021-05-25 09:01:34 -0700

    update

--- 046/src/main.go
+++ 046/src/main.go
@@ -1,8 +1,8 @@
 package main
 
-var ret [][]int{{}}
+// walk path
+var ret [][]int
 var stack map[int]bool
-
 
 func travel(nums []int, s map[int]bool) {
 	if len(nums) == len(s) {
@@ -11,29 +11,51 @@ func travel(nums []int, s map[int]bool) {
 			ts = append(ts, k)
 		}
 		ret = append(ret, ts)
+		return
 	}
-	
-	for k, v := range nums {
+
+	for _, v := range nums {
 		if _, ok := stack[v]; ok {
 			continue
 		}
 		stack[v] = true
 		travel(nums, stack)
+		// back one step
 		delete(stack, v)
 	}
 
 }
 
-func Permute(nums []int) [][]int {
+func PermuteTravel(nums []int) [][]int {
 	ret = make([][]int, 0)
-	for k, v := range nums {
-		if _, ok := stack[v]; ok {
-			continue
-		}
-		stack[v] = true
+	stack = make(map[int]bool)
 
+	travel(nums, stack)
+	return ret
 
+}
+
+//////////////////////////recursion/////////////////////////////////////////////
+var ret [][]int
+
+func PermuteRecursion(nums []int) [][]int {
+	if len(nums) == 1 {
+		ret := make([][]int, 0)
+		ret = append(ret, nums)
+		return ret
 	}
 
-    
+	ret := make([][]int, 0)
+	for k, v := range nums {
+		a := []int{}
+		a = append(a, nums[:k]...)
+		a = append(a, nums[k+1:]...)
+		child := Permute(a)
+		for _, cv := range child {
+			ctmp := []int{v}
+			ctmp = append(ctmp, cv...)
+			ret = append(ret, ctmp)
+		}
+	}
+	return ret
 }
--- 046/src/main_test.go
+++ 046/src/main_test.go
@@ -5,32 +5,11 @@ import (
 	"testing"
 )
 
-func TestFindMedianSortedArrays(t *testing.T) {
+func TestPermute(t *testing.T) {
 	fmt.Println("===========Case 1============")
-	a := []int{2}
-	b := []int{1, 3}
-	ret := FindMedianSortedArrays(a, b)
+	b := []int{1, 2, 3}
+	ret := Permute(b)
 
-	if ret != 2 {
-		t.Errorf("expect 2 but received %f", ret)
-	}
-
-	fmt.Println("===========Case 2============")
-	a = []int{}
-	b = []int{1}
-	ret = FindMedianSortedArrays(a, b)
-
-	if ret != 1 {
-		t.Errorf("expect 1 but received %f", ret)
-	}
-
-	fmt.Println("===========Case 3============")
-	a = []int{1, 2}
-	b = []int{3, 4}
-	ret = FindMedianSortedArrays(a, b)
-
-	if ret != 2.5 {
-		t.Errorf("expect 1 but received %f", ret)
-	}
+	fmt.Println(ret)
 
 }
