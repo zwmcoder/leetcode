5d9c612a4e63aafd6ced16c477fc2e88b113c84f
zwmcoder <zhangwm1987@163.com>
2021-05-26 07:58:21 -0700

    051

---
+++ 051/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSolveNQueens(t *testing.T) {
+	fmt.Println("Test########################")
+	ret := SolveNQueens(4)
+	fmt.Println(ret)
+
+}
